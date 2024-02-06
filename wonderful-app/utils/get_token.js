017ab961b894553f114862cfb4b3fd2affda2de2
MkFdez <mirkofs29@gmail.com>
2024-02-06 15:41:06 -0500

    try

--- wonderful-app/utils/get_token.js
+++
@@ -1,9 +0,0 @@
-
-export default function getTokenFrom(request){
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-      return authorization.substring(7)
-    }
-    return null
-  }
-
