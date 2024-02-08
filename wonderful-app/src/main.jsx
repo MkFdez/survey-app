2b893649915b7a8e97b18f371fe2a9b60d247aed
MkFdez <mirkofs29@gmail.com>
2024-02-07 22:12:09 -0500

    ya no se q hacer

--- wonderful-app/src/main.jsx
+++ wonderful-app/src/main.jsx
@@ -13,12 +13,12 @@ import {Provider} from 'react-redux'
 const test = [{p: "Select the correct Answer \n 2+2 is:", a:[{type:"i", value:'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'},{type:"i", value:'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'},{type:"i", value:'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'},{type:"i", value:'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'}]}, {p: "Select the correct Answer \n 7+2 is:", a:[{type:"t", value:8},{type:"t", value:0},{type:"t", value:20},{type:"t", value:2}]}] 
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
-  <HashRouter>
+  <BrowserRouter>
   <Provider store={store}>
       <ChakraProvider theme={darkTheme}>      
         <App />     
       </ChakraProvider>
     </Provider>
-  </HashRouter>
+  </BrowserRouter>
 )
 
