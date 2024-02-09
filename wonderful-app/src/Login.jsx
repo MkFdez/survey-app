1c5ce822c3f2d43c576c7a07f57d02e2e973bb57
MkFdez <mirkofs29@gmail.com>
2024-02-08 22:37:54 -0500

    debug4

--- wonderful-app/src/Login.jsx
+++ wonderful-app/src/Login.jsx
@@ -51,7 +51,8 @@ import API_URL from '../config/backend';
         const {token} = response.data;
         const {picture} = response.data;
         const cookie = new Cookie();
-        console.log(`response - ${response}`)
+        console.log(`response - ${JSON.stringify(response)}`)
+        
         if(!rememberMe) {
             const expirationDate = new Date();
             // Set the cookie with an expiration time of 30 day from the current time
