c1e50c84cbcb672e60b088122cfe51ec6157d533
MkFdez <mirkofs29@gmail.com>
2024-02-09 21:33:00 -0500

    11

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(cors({
         if (origin == 'https://surveyswebsite.onrender.com' ) {
             callback(null, true)
         } else {
-            callback(new Error('Not allowed by CORS'))
+            callback(new Error(`Not allowed by CORS ${origin}`))
         }
     },
     credentials: true,
