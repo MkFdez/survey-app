044c96008d17b642963e5f9a470f2d7280cf8566
MkFdez <mirkofs29@gmail.com>
2024-02-09 21:14:57 -0500

    crazy try No9

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(cors({
     credentials: true,
     optionsSuccessStatus: 200
 }));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 //app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
