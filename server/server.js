2f1a12c5a14dcb5b7d8dbc6373b248118b6cade3
MkFdez <mirkofs29@gmail.com>
2024-02-10 21:26:13 -0500

    11.7

--- server/server.js
+++ server/server.js
@@ -14,26 +14,9 @@ const corsOptions = require("./config/corsOptions")
 const PORT = process.env.PORT || 3030;
 const app = express()
 //app.use(cors(corsOptions));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.header("Access-Control-Allow-Credentials", true); 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  next();
-});
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(bodyParser.text())
+app.use(cors(corsOptions))
 
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.json())
 
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
