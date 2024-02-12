79edf29111e994b0f300b7cf9d69e9e7b0eaa596
MkFdez <mirkofs29@gmail.com>
2024-02-12 12:17:55 -0500

    jklads

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -3,7 +3,7 @@ const allowedOrigins = require('./allowedOrigins')
 const corsOptions = {
     origin: (origin, callback) => {
         console.log(`origin-${origin}`)
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        if (true) {
             callback(null, true)
         } else {            
             callback(new Error(`Not allowed by CORS--${origin}`))
