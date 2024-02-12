711b8b846418bd8149502df67ccaa26a358fad2b
MkFdez <mirkofs29@gmail.com>
2024-02-12 12:11:51 -0500

    P6

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
-        console.log(origin)
+        console.log(`origin-${origin}`)
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {            
