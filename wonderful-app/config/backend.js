e5f6b5dba864626a2266574c781533a79bac345e
MkFdez <mirkofs29@gmail.com>
2024-02-12 21:15:41 -0500

    FInally

--- wonderful-app/config/backend.js
+++ wonderful-app/config/backend.js
@@ -1,4 +1,4 @@
 export default function API_URL(){
-    const api_url = "https://surveybackend-r4pd.onrender.com/";
+    const api_url = "https://surveybackend-r4pd.onrender.com";
     return api_url;
 }
